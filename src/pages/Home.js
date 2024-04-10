8aca94e045bdde1c82e70636e238fd6fc025d960
Himanshi Sherwani <himanshi2301s@gmail.com>
2024-04-10 22:15:41 +0530

    added loading images for suspense

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React,{Suspense} from "react";
+import loadingGif from "../images/loadingGif.gif"
 const Navbar = React.lazy(() => import("../components/Navbar/Navbar"));
 const Content = React.lazy(() => import("../components/Content/Content"));
 const Footer = React.lazy(() => import("../components/Footer/Footer"));
@@ -6,9 +7,11 @@ const Footer = React.lazy(() => import("../components/Footer/Footer"));
 const Home = () => {
   return (
     <div className="container-fluid">
+      <Suspense fallback={<img src={loadingGif} style={{padding:'500px 900px'}} alt="Loading..." />}>
       <Navbar />
       <Content />
       <Footer />
+      </Suspense>
     </div>
   );
 };
